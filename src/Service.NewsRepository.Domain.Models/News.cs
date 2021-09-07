e31b70ad2c0af167ed9b9fa3629e1ea16c6ba241
Alexey <alexey.p@smplt.net>
2021-09-07 12:03:44 +0300

    update News dto

--- src/Service.NewsRepository.Domain.Models/News.cs
+++ src/Service.NewsRepository.Domain.Models/News.cs
@@ -7,11 +7,13 @@ namespace Service.NewsRepository.Domain.Models
     [DataContract]
     public class News
     {
-        [DataMember(Order = 1)]public string Source { get; set; }
-        [DataMember(Order = 2)]public string Topic { get; set; }
-        [DataMember(Order = 3)]public string Lang { get; set; }
-        [DataMember(Order = 4)]public DateTime Timestamp { get; set; }
-        [DataMember(Order = 5)]public string UrlAddress { get; set; }
+        [DataMember(Order = 1)] public string Source { get; set; }
+        [DataMember(Order = 2)] public string Topic { get; set; }
+        [DataMember(Order = 3)] public string Lang { get; set; }
+        [DataMember(Order = 4)] public DateTime Timestamp { get; set; }
+        [DataMember(Order = 5)] public string UrlAddress { get; set; }
         [DataMember(Order = 6)] public List<string> AssociatedAssets { get; set; } = new();
+        [DataMember(Order = 7)] public string ImageUrl { get; set; }
+        [DataMember(Order = 8)] public string Sentiment { get; set; }
     }
 }
