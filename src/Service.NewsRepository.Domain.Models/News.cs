3b1c04fd45f44582326fca543b88d58d805d1cd3
Alexey <alexey.p@smplt.net>
2021-09-08 12:31:06 +0300

    update news dto

--- src/Service.NewsRepository.Domain.Models/News.cs
+++ src/Service.NewsRepository.Domain.Models/News.cs
@@ -9,11 +9,12 @@ namespace Service.NewsRepository.Domain.Models
     {
         [DataMember(Order = 1)] public string Source { get; set; }
         [DataMember(Order = 2)] public string Topic { get; set; }
-        [DataMember(Order = 3)] public string Lang { get; set; }
-        [DataMember(Order = 4)] public DateTime Timestamp { get; set; }
-        [DataMember(Order = 5)] public string UrlAddress { get; set; }
-        [DataMember(Order = 6)] public List<string> AssociatedAssets { get; set; } = new();
-        [DataMember(Order = 7)] public string ImageUrl { get; set; }
-        [DataMember(Order = 8)] public string Sentiment { get; set; }
+        [DataMember(Order = 3)] public string Description { get; set; }
+        [DataMember(Order = 4)] public string Lang { get; set; }
+        [DataMember(Order = 5)] public DateTime Timestamp { get; set; }
+        [DataMember(Order = 6)] public string UrlAddress { get; set; }
+        [DataMember(Order = 7)] public List<string> AssociatedAssets { get; set; } = new();
+        [DataMember(Order = 8)] public string ImageUrl { get; set; }
+        [DataMember(Order = 9)] public string Sentiment { get; set; }
     }
 }
